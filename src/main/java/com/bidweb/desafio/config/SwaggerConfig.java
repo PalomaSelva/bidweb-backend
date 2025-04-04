d0ef103d035c7a03898fdbbbe6c830781d594686
PalomaSelva <palomaselva1@email.com>
2025-04-04 17:04:16 -0300

    chore: configurar Swagger para documentação da API

---
+++ src/main/java/com/bidweb/desafio/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.bidweb.desafio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                                .title("Vendas API")
+                                .description("Sistema de monitoramento de vendas")
+                                .version("1.0"));
+    }
+}
