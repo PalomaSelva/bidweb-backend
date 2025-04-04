1dadd2c8b5dc5d99ee9e8e49c77c710fd2f490d6
PalomaSelva <palomaselva1@email.com>
2025-04-04 20:20:09 -0300

    feat: adição do método findByEmail no repositório UserRerpository

--- src/main/java/com/bidweb/desafio/repository/UserRerpository.java
+++ src/main/java/com/bidweb/desafio/repository/UserRerpository.java
@@ -1,9 +1,11 @@
 package com.bidweb.desafio.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bidweb.desafio.model.User;
 
 public interface UserRerpository extends JpaRepository<User,Long>{
-  
+  Optional<User> findByEmail(String email);
 }
