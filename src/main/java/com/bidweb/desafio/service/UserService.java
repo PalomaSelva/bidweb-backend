492fa3a385f535c8802fad3a8e81d1b55365ab34
PalomaSelva <palomaselva1@email.com>
2025-04-04 20:27:00 -0300

    feat: criação do UserService com método de criação de usuário

---
+++ src/main/java/com/bidweb/desafio/service/UserService.java
@@ -0,0 +1,38 @@
+package com.bidweb.desafio.service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bidweb.desafio.dto.UserRequest;
+import com.bidweb.desafio.dto.UserResponse;
+import com.bidweb.desafio.model.User;
+import com.bidweb.desafio.repository.UserRerpository;
+
+@Service
+public class UserService {
+
+@Autowired
+private UserRerpository userRepository;
+
+public UserResponse createUser(UserRequest user) {
+  try {
+    Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+    if (existingUser.isPresent()) {
+      throw new RuntimeException("Usuário já cadastrado");
+    }
+    User newUser = new User();
+    newUser.setName(user.getName());
+    newUser.setEmail(user.getEmail());
+    newUser.setPassword(user.getPassword());
+    newUser.setCreatedAt(LocalDateTime.now());
+    newUser.setUpdatedAt(LocalDateTime.now());
+    userRepository.save(newUser);
+    return new UserResponse(newUser);
+  } catch (Exception ex) {
+    throw new RuntimeException("Erro ao criar usuário: " + ex.getMessage());
+  }
+}
+}
