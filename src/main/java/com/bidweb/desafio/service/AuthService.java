320a052afe887a16ab09a31e3fb3f6a6c4f539d7
PalomaSelva <palomaselva1@email.com>
2025-04-04 23:29:30 -0300

    feat: implementação da verificação de senha no método de login da classe AuthService

--- src/main/java/com/bidweb/desafio/service/AuthService.java
+++ src/main/java/com/bidweb/desafio/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.bidweb.desafio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bidweb.desafio.dto.AuthRequest;
@@ -11,9 +12,15 @@ public class AuthService {
   @Autowired
   private UserRerpository userRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public void login(AuthRequest request) {
     var user = userRepository.findByEmail(request.getEmail())
         .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+      throw new RuntimeException("Senha inválida");
+    }
   }
 }
